96e6cab6c9624d091f44b0fd2ef3b0ac6ab1f01a
Loic Dupong <loic@dupong.be>
2025-06-03 11:15:19 +0200

    Display shows + btns

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,83 @@
-const inputSearch = document.getElementById('search');
-const btnSearch = document.querySelector('.btn--search');
-const resultsHTML = document.querySelector('.swiper__results');
+const resultsHTML = document.querySelector('.wrapper--results');
+const wrapperBtn = document.querySelector('.wrapper--btn');
+const showDetailsHTML = document.querySelector('.show__details');
+let showContainer;
+
 const apiKey = `6631e5f1dc96088e0d26b86da29b5b6a`;
 let imgUrl = `https://image.tmdb.org/t/p/w500`;
 let defautImg = `https://motivatevalmorgan.com/wp-content/uploads/2016/06/default-movie-1-300x450.jpg`
-let movies;
+let imgPath;
 
-async function getAPI(query) {
+// == Fetch API ==
+async function getAPI(filter) {
   try {
-      const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${apiKey}&include_adult=false&language=en-US&page=1`);
-      const data = await response.json();
-      return data.results;
+      const response = await fetch(`https://api.themoviedb.org/3/tv/${filter}?api_key=${apiKey}&include_adult=false&language=en-US`);
+      const dataID = await response.json();
+      const data = dataID.results;
+      return {
+        data,
+        dataID,
+      }
   } catch (error) {
       alert('Erreur :', error);
   }
 }
 
-async function displayMoviez(query) {
+// == Display Shows ==
+async function displayShows(showFilter, targetBtn) {
     resultsHTML.innerHTML = "";
-    movies = await getAPI(query)
-    const popArray =  movies.sort((a, b) => b.popularity - a.popularity);
-    for (const movie of popArray){
-        let imgPath = movie.poster_path ? `${imgUrl}${movie.poster_path}` : `${defautImg}`;
-        const movieContainer = document.createElement('div');
-        movieContainer.classList = "swiper-slide movie__single";
-        movieContainer.innerHTML += `
-        <h2 class="movie__title">${movie.original_title}</h2>
-        <div class="movie__img"><img src="${imgPath}" alt=""></div>
-        `
-        resultsHTML.append(movieContainer);
-        console.log(movie.popularity);
+    const { data, dataID } = await getAPI(showFilter);
+    const btns = document.querySelectorAll('.btn');
+    for (const btn of btns){
+        btn.classList.contains('btn--is-active') ? btn.classList.remove('btn--is-active') : null;
     }
-    swiper.update();
+    targetBtn.classList.add('btn--is-active');
+
+    for (const show of data){
+        imgPath = show.poster_path ? `${imgUrl}${show.poster_path}` : `${defautImg}`;
+        showContainer = document.createElement('div');
+        showContainer.classList = "show__single";
+        showContainer.dataset.id = show.id;
+        showContainer.innerHTML += `
+        <h2 class="show__title">${show.name}</h2>
+        <div class="show__img"><img src="${imgPath}" alt=""></div>
+        <div class="show__rating">${(show.vote_average).toFixed(1)}</div>
+        `
+        resultsHTML.append(showContainer);
+    }
 }
 
-btnSearch.addEventListener('click', async (e) => {
-    e.preventDefault();
-    await displayMoviez(inputSearch.value);
+// == Display Show Details PopUp
+async function displayShowDetails(showID) {
+    showDetailsHTML.innerHTML = "";
+    const { data, dataID }= await getAPI(showID)
+    imgPath = dataID.poster_path ? `${imgUrl}${dataID.poster_path}` : `${defautImg}`;
+    showDetailsHTML.innerHTML += `
+    <div class="show__img show__img--details"><img src="${imgPath}" alt=""></div>
+    <div class="show__infos">
+       <h2 class="show__title show__title--details">${dataID.name}</h2> 
+       <p class="show__overview">${dataID.overview}</p>
+    </div>
+    `
+}
+
+
+// == Initial display ==
+displayShows("top_rated", document.querySelector('.btn--top-rated'));
+
+
+// == Event Listener btns ==
+wrapperBtn.addEventListener('click', async (e)=>{
+    if (e.target.classList.contains('btn')) {
+        const filter = e.target.dataset.filter;
+        await displayShows(filter, e.target);
+    }
 })
+
+resultsHTML.addEventListener('click', async (e)=>{
+    if (e.target.classList.contains('show__single')) {
+        const id = e.target.dataset.id;
+        await displayShowDetails(id);
+    }
+})
+
